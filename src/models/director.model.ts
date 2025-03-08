d9dc8276229161fd72b6944c0adb336504b0f898
Omer Aslan <32545727+qouv@users.noreply.github.com>
2025-03-08 15:56:34 +0300

    refactor: add validation

--- src/models/director.model.ts
+++ src/models/director.model.ts
@@ -8,10 +8,34 @@ export interface IDirector extends Document {
 }
 
 const directorSchema = new Schema<IDirector>({
-	firstName: { type: String, required: true },
-	secondName: { type: String, required: true },
-	birthDate: { type: Date, required: true },
-	bio: { type: String, required: true },
+	firstName: {
+		type: String,
+		required: [true, 'First name is required'],
+		maxlength: [64, 'First name cannot be more than 64 characters']
+	},
+	secondName: {
+		type: String,
+		required: [true, 'Second name is required'],
+		maxlength: [64, 'Second name cannot be more than 64 characters']
+	},
+	birthDate: {
+		type: Date,
+		required: [true, 'Birth date is required'],
+		validate: [
+			{
+				validator: function(value: Date) {
+					// Ensure birthDate is not in the future
+					return value <= new Date();
+				},
+				message: 'Birth date cannot be in the future'
+			}
+		]
+	},
+	bio: {
+		type: String,
+		required: [true, 'Bio is required'],
+		maxLength: [400, 'Bio cannot be more than 400 characters']
+	},
 }, { timestamps: true });
 
 export const Director = mongoose.model<IDirector>('Director', directorSchema);
