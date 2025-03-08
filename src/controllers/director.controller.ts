0f48be376546593a33ef03643a5920cf7c2db512
Omer Aslan <32545727+qouv@users.noreply.github.com>
2025-03-08 15:56:14 +0300

    feat: add create director

---
+++ src/controllers/director.controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response, NextFunction } from 'express'
+import { DirectorService } from '../services/director.service'
+
+export class DirectorController {
+	private directorService: DirectorService;
+
+	constructor() {
+		this.directorService = new DirectorService();
+	}
+
+	async createDirector(req: Request, res: Response, next: NextFunction): Promise<void> {
+		try {
+			await this.directorService.createDirector(req.body)
+
+			res.sendStatus(201)
+		} catch (error) {
+			next(error)
+		}
+	}
+}
