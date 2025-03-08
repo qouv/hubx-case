a884fad5066c01270c15335198309516c6298472
Omer Aslan <32545727+qouv@users.noreply.github.com>
2025-03-08 15:53:49 +0300

    refactor: add details to error response

--- src/middlewares/error.middleware.ts
+++ src/middlewares/error.middleware.ts
@@ -1,12 +1,14 @@
 import { Request, Response, NextFunction } from 'express'
 import { handleError } from '../utils/error.utils'
 
-export const errorMiddleware = (
+const errorMiddleware = (
 	error: unknown,
 	_req: Request,
 	res: Response,
 	_next: NextFunction
 ): void => {
-	const { message, statusCode } = handleError(error)
-	res.status(statusCode).json({ message })
+	const { message, statusCode, details } = handleError(error)
+	res.status(statusCode).json({ message, details })
 };
+
+export default errorMiddleware
--- src/utils/error.utils.ts
+++ src/utils/error.utils.ts
@@ -9,7 +9,7 @@ export class AppError extends Error {
 	}
 }
 
-export const handleError = (error: unknown): { message: string; statusCode: number } => {
+export const handleError = (error: unknown): { message: string; statusCode: number; details?: any } => {
 	if (error instanceof AppError) {
 		return {
 			message: error.message,
@@ -18,6 +18,20 @@ export const handleError = (error: unknown): { message: string; statusCode: numb
 	}
 
 	if (error instanceof Error) {
+		if (error.name === "ValidationError") {
+			// Extract and format validation error messages from Mongoose
+			const errors = Object.fromEntries(
+				Object.entries((error as any).errors).map(([key, value]) => [key, (value as { message: string }).message.replace(/^Path `|`/g, "").trim()])
+			);
+
+			return {
+				message: "Validation failed",
+				statusCode: 400,
+				details: errors,
+			};
+		}
+
+
 		return {
 			message: error.message,
 			statusCode: 400,
