b29da48aa6dcb9a503cc1a3301416e00d52fd846
Omer Aslan <32545727+qouv@users.noreply.github.com>
2025-03-10 15:43:12 +0300

    refactor: change duplicate imdb id check

--- src/models/movie.model.ts
+++ src/models/movie.model.ts
@@ -11,21 +11,21 @@ export interface IMovie extends Document {
 	director: mongoose.Types.ObjectId | IDirector;
 }
 
-const validGenres = ['Action', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Romance', 'Thriller', 'Western'];
+const validGenres = ['Action', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Romance', 'Thriller', 'Western']
 
 const movieSchema = new Schema<IMovie>({
-	title: { type: String, required: true },
-	description: { type: String, required: true },
-	releaseDate: { type: Date, required: true },
-	genre: [{ type: String, required: true, enum: validGenres }],
-	rating: { type: Number, required: true, min: 0, max: 10 },
+	title: { type: String, required: [true, 'Title is required'] },
+	description: { type: String, required: [true, 'Description is required'] },
+	releaseDate: { type: Date, required: [true, 'Release date is required'] },
+	genre: [{ type: String, required: [true, 'Genre is required'], enum: validGenres }],
+	rating: { type: Number, required: [true, 'Rating is required'], min: 0, max: 10 },
 	imdbId: {
 		type: String,
-		required: true,
+		required: [true, 'IMDB ID is required'],
 		unique: [true, 'IMDB ID must be unique'],
 		match: [/^tt\d{7,10}$/, 'IMDB ID must match the pattern tt followed by 7 to 10 digits']
 	},
-	director: { type: Schema.Types.ObjectId, ref: 'Director', required: true }
-}, { timestamps: true });
+	director: { type: Schema.Types.ObjectId, ref: 'Director', required: [true, 'Director is required'] }
+}, { timestamps: true })
 
-export const Movie = mongoose.model<IMovie>('Movie', movieSchema);
+export const Movie = mongoose.model<IMovie>('Movie', movieSchema)
